14a52ac2c3244542b9e42f5dd337b6992f817263
ttateluc <97220909+ttateluc@users.noreply.github.com>
2023-02-21 09:00:35 -0600

    Update post.js

--- server/mongodb/models/post.js
+++ server/mongodb/models/post.js
@@ -1,11 +1,11 @@
-import mongoose from 'mongoose';
+import mongoose from 'mongoose'; // Defines schema
 
-const Post = new mongoose.Schema({
+const Post = new mongoose.Schema({ // New schema
   name: { type: String, required: true },
   prompt: { type: String, required: true },
   photo: { type: String, required: true },
 });
 
-const PostSchema = mongoose.model('Post', Post);
+const PostSchema = mongoose.model('Post', Post); // New model
 
 export default PostSchema;
