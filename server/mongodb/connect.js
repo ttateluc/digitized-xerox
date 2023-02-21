0fa0717fe42dcb766463c7251740de96910549f3
ttateluc <97220909+ttateluc@users.noreply.github.com>
2023-02-21 09:01:11 -0600

    Update connect.js

--- server/mongodb/connect.js
+++ server/mongodb/connect.js
@@ -1,10 +1,10 @@
 import mongoose from 'mongoose';
 
-const connectDB = (url) => {
-  mongoose.set('strictQuery', true);
-  mongoose.connect(url)
-    .then(() => console.log('connected to mongo'))
-    .catch((err) => {
+const connectDB = (url) => { // Takes a URL for the MongoDB database to connect to
+  mongoose.set('strictQuery', true); // Throws an error when a query is made with a field that is not defined in the schema
+  mongoose.connect(url) // Connects to the MongoDB database specified by the url parameter
+    .then(() => console.log('connected to mongo')) // If the connection is successful, this logs a message to the console
+    .catch((err) => { // If the connection fails, this logs an error message and the error object to the console
       console.error('failed to connect with mongo');
       console.error(err);
     });
